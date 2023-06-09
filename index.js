c360bbc8bcac63bca362a30f6547c58e8920fe59
jotatdf <juangoitia3@gmail.com>
2023-06-08 21:29:46 -0300

    prueba github

---
+++ index.js
@@ -0,0 +1,8 @@
+const miBoton = document.getElementById('miBoton');
+function handleClick() {
+    alert('¡Hola! Has hecho click en el botón.');
+   }
+   miBoton.addEventListener('click', handleClick);
+   function showAlert(){
+    alert('Hola, esta es una alerta desde Java Script');
+}
